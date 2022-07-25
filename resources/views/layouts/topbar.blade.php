3d1663a5bc540e5d1564480dd7f1385c7c2f2329
Administrator <fit.vanson@gmail.com>
2022-07-25 15:58:28 +0700

    update

--- resources/views/layouts/topbar.blade.php
+++ resources/views/layouts/topbar.blade.php
@@ -12,26 +12,6 @@
                         <img src="{{ URL::asset('/assets/images/logo-light.png') }}" alt="" height="30">
                     </span>
                 </a>
-
-{{--                <div class="dropdown">--}}
-{{--                    <button type="button" class="btn waves-effect" id="page-header-user-dropdown"--}}
-{{--                            data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">--}}
-{{--                        <img class="rounded-circle header-profile-user" style="width: 100px; height: 100px"  src="{{ URL::asset('/assets/images/users/user-4.jpg') }}"--}}
-{{--                             alt="Header Avatar">--}}
-{{--                    </button>--}}
-{{--                    <div class="dropdown-menu dropdown-menu-right">--}}
-{{--                        <!-- item-->--}}
-{{--                        <a class="dropdown-item" href="#"><i class="mdi mdi-account-circle font-size-17 align-middle mr-1"></i> @lang('translation.Profile')</a>--}}
-{{--                        <div class="dropdown-divider"></div>--}}
-{{--                        <a class="dropdown-item text-danger" href="javascript:void();" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">--}}
-{{--                            <i class="bx bx-power-off font-size-17 align-middle mr-1 text-danger"></i>--}}
-{{--                            @lang('translation.Logout')--}}
-{{--                        </a>--}}
-{{--                        <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">--}}
-{{--                            @csrf--}}
-{{--                        </form>--}}
-{{--                    </div>--}}
-{{--                </div>--}}
             </div>
 
 
@@ -53,28 +33,8 @@
                     @if(isset($page_title))
                     <button type="button" class="btn btn-primary waves-effect waves-light create{{preg_replace('/\s+/','',$page_title)}}">Create</button>
                     @endif
-{{--                    <a class="btn btn-primary" href="#" role="button" id="create{{preg_replace('/\s+/','',$page_title)}}">Create</a>--}}
                 </div>
             </div>
-{{--            <div class="dropdown d-inline-block">--}}
-{{--                <button type="button" class="btn header-item waves-effect" id="page-header-user-dropdown"--}}
-{{--                    data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">--}}
-{{--                    <img class="rounded-circle header-profile-user" src="{{ URL::asset('/assets/images/users/user-4.jpg') }}"--}}
-{{--                        alt="Header Avatar">--}}
-{{--                </button>--}}
-{{--                <div class="dropdown-menu dropdown-menu-right">--}}
-{{--                    <!-- item-->--}}
-{{--                    <a class="dropdown-item" href="#"><i class="mdi mdi-account-circle font-size-17 align-middle mr-1"></i> @lang('translation.Profile')</a>--}}
-{{--                    <div class="dropdown-divider"></div>--}}
-{{--                    <a class="dropdown-item text-danger" href="javascript:void();" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">--}}
-{{--                        <i class="bx bx-power-off font-size-17 align-middle mr-1 text-danger"></i>--}}
-{{--                        @lang('translation.Logout')--}}
-{{--                    </a>--}}
-{{--                    <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">--}}
-{{--                        @csrf--}}
-{{--                    </form>--}}
-{{--                </div>--}}
-{{--            </div>--}}
         </div>
     </div>
 </header>
