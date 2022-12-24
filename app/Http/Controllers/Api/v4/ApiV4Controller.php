e3ad32be75f8d5b4a5fa49bef1ca53974348dbfc
Administrator <sonvt.vietmmo@gmail.com>
2022-12-24 16:24:49 +0000

    update

--- app/Http/Controllers/Api/v4/ApiV4Controller.php
+++ app/Http/Controllers/Api/v4/ApiV4Controller.php
@@ -67,18 +67,20 @@ class ApiV4Controller extends Controller
 
 
         $ads[] = [
+            "ad_id"=> 5,
+            "ads_name"=> "Wortise",
             'ads_info' =>[
-                'publisher_id' => @$site_ads['AdMob_Publisher_ID'],
-                'banner_on_off' => @$status_ads,
-                'banner_id' => @$site_ads['AdMob_Banner_Ad_Unit_ID'],
-                'interstitial_on_off' => @$status_ads,
-                'native_on_off' => @$status_ads,
-                'interstitial_id' => @$site_ads['AdMob_Interstitial_Ad_Unit_ID'],
-                'native_id' => @$site_ads['AdMob_Native_Ad_Unit_ID'],
+                'publisher_id' => "c2f94dbb-e29a-4a95-bbf9-fbf860c428b3",
+                'banner_on_off' =>"1",
+                'banner_id' =>"acbb8bfe-be23-4252-a173-3bbc0d8dfb8a",
+                'interstitial_on_off' => "1",
+                'native_on_off' => "1",
+                'interstitial_id' => "70f3bdd5-4cd1-4943-a34d-9d23ba25542d",
+                'native_id' => "f78f3300-dc1b-4ece-b85b-2bc5ecbca8bb",
                 'interstitial_clicks' => 5,
                 'native_position' => 5,
             ],
-            'status' =>true,
+            'status' =>'true',
         ];
         $page_list = [
             [
