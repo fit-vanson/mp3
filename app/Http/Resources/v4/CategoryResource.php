17c37f3dd98e638f692de4230faf7ce4e5cf27fd
Administrator <sonvt.vietmmo@gmail.com>
2022-12-26 09:24:10 +0700

    update

---
+++ app/Http/Resources/v4/CategoryResource.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Resources\v4;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class CategoryResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array|\Illuminate\Contracts\Support\Arrayable|\JsonSerializable
+     */
+    public function toArray($request)
+    {
+        return [
+            'post_id' => $this->id,
+            'post_type' => 'category',
+            'post_title' => $this->category_name ,
+            'post_image' => $this->category_image ?  asset('storage/sites/'.$this->site_id.'/categories/'.$this->category_image) : asset('storage/default.png') ,
+
+
+        ];
+    }
+}
