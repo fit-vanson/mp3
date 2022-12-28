8e04054a55272683a0f42521f0d2163255260218
Administrator <sonvt.vietmmo@gmail.com>
2022-12-28 08:47:55 +0700

    update

--- app/Http/Controllers/MusicsController.php
+++ app/Http/Controllers/MusicsController.php
@@ -601,9 +601,9 @@ class MusicsController extends Controller
                 [
                     'music_url_link_audio_ytb'=>$value['url_audio'],
                     'music_view_count'=>$value['viewCount'],
-                    'music_description'=> ($value['description']),
+//                    'music_description'=> ($value['description']),
                     'music_title'=> ($value['title']),
-                    'music_keywords'=> ($value['keywords']),
+//                    'music_keywords'=> ($value['keywords']),
                     'music_thumbnail_link'=>  "https://i.ytimg.com/vi_webp/$key/mqdefault.webp",
                     'expire' => time(),
                 ]
