20015880c9f7c8adcd6fac6135525b0ee32acd9e
Administrator <sonvt.vietmmo@gmail.com>
2022-12-28 08:43:00 +0700

    update

--- app/Http/Resources/v4/MusicFeatureResource.php
+++ app/Http/Resources/v4/MusicFeatureResource.php
@@ -18,7 +18,7 @@ class MusicFeatureResource extends JsonResource
         return [
             'slider_id' => $this->categories[rand(0,count($this->categories)-1)]->id,
             'slider_title' => ($this->music_title) ,
-            'slider_info' => substr(($this->music_description),0,30),
+            'slider_info' => substr($this->music_description,0,30),
             'songs_ids' => "",
             'slider_image' => $this->music_thumbnail_link ,
         ];
